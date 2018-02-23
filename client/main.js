70f0f13ce9c59b2506d31c117cea4dcbeb48d914
EC2 Default User <ec2-user@ip-172-31-68-98.ec2.internal>
2018-02-23 04:10:49 +0000

    20180223-2nd-beforeMailgun

--- client/main.js
+++ client/main.js
@@ -20,3 +20,83 @@
 //     instance.counter.set(instance.counter.get() + 1);
 //   },
 // });
+
+Template.home.rendered = function() {
+  $("#fullText1").hide();
+  $("#fullText2").hide();
+  $("#fullText3").hide();
+  $("#scheduleAppForms").hide();
+};
+
+Template.home.events({
+  "click #scheduleAppBtn":function(event){
+    
+    $("#scheduleAppForms").show(1500);
+    // $("#scheduleAppForms").slideDown();
+    $("#scheduleAppBtn").hide(1000);
+  },
+  
+  "click #cancelAppBtn":function(event){
+    $("#scheduleAppBtn").show(1500);
+    $("#scheduleAppForms").hide(1000);
+  },
+  
+  
+  
+  "click #toggle1":function(event){
+    var elem = $("#toggle1").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#excerptText1").removeClass("excerptText");
+      $("#excerptText1").addClass("fullText");
+      
+      $("#toggle1").text("Read Less");
+      $("#fullText1").slideDown(2000);
+      
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#excerptText1").removeClass("fullText");
+      $("#excerptText1").addClass("excerptText");
+      $("#toggle1").text("Read More");
+      $("#fullText1").slideUp(2000);
+    }
+  },
+  
+  "click #toggle2":function(event){
+    var elem = $("#toggle2").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#excerptText2").removeClass("excerptText");
+      $("#excerptText2").addClass("fullText");
+      
+      $("#toggle2").text("Read Less");
+      $("#fullText2").slideDown(2000);
+      
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#excerptText2").removeClass("fullText");
+      $("#excerptText2").addClass("excerptText");
+      $("#toggle2").text("Read More");
+      $("#fullText2").slideUp(2000);
+    }
+  },
+  
+  "click #toggle3":function(event){
+    var elem = $("#toggle3").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#excerptText3").removeClass("excerptText");
+      $("#excerptText3").addClass("fullText");
+      
+      $("#toggle3").text("Read Less");
+      $("#fullText3").slideDown(2000);
+      
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#excerptText3").removeClass("fullText");
+      $("#excerptText3").addClass("excerptText");
+      $("#toggle3").text("Read More");
+      $("#fullText3").slideUp(2000);
+    }
+  },
+});
